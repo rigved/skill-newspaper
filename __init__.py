a47b7ab7b86b25e094e1617dc7ae469e0a0c9812
Rigved Rakshit <r.phate@gmail.com>
2020-03-11 00:00:48 +0530

    Added code to generate summary from a given webpage URL

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 from mycroft import MycroftSkill, intent_file_handler
 
+import mechanicalsoup
+from gensim.summarization.summarizer import summarize
+
 
 class WebpageSummarizer(MycroftSkill):
     def __init__(self):
@@ -12,4 +15,19 @@ class WebpageSummarizer(MycroftSkill):
 
 def create_skill():
     return WebpageSummarizer()
+
+
+def get_summary(url):
+    """
+    Takes a website URL and returns the URL title and a summary of the website content.
+    :param url: Website URL.
+    :return: Website title and summarized website text.
+    """
+    browser = mechanicalsoup.StatefulBrowser(
+        user_agent='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0')
+    browser.open(url)
+    page = browser.get_current_page()
+    website_text = ' '.join(map(lambda p: p.text, page.find_all('p')))
+    summarized_text = summarize(website_text)
+    return page.title.text, summarized_text
 
