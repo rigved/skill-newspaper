a9c09b741eb31ab25b89f50ab3f664b92b7b4317
Rigved Rakshit <r.phate@gmail.com>
2020-03-22 18:13:14 +0530

    Revert "Rename Django's manage.py so that the Mycroft Skill Loader ignores it"
    This reverts commit f972fb8c942e4295083859ca2c332a18e22f2f4b.

--- scripts/update_password_and_token.sh
+++ scripts/update_password_and_token.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}
 strings /dev/urandom | grep -o '[[:graph:]]' | head -n 128 | tr -d '\n' | head -c 64 | xargs -0 -I{} ./scripts/update_superuser_password.exp {}
 cd ../apiv1/
-python manage drf_create_token -r mycroftai
+python manage.py drf_create_token -r mycroftai
