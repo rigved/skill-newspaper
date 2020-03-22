37b5654638fcb9d23089dae082fc24754141fdd5
Rigved Rakshit <r.phate@gmail.com>
2020-03-22 18:45:20 +0530

    Revert "Revert "Rename Django's manage.py so that the Mycroft Skill Loader ignores it""
    This reverts commit a9c09b741eb31ab25b89f50ab3f664b92b7b4317.

--- scripts/create_database_and_superuser.sh
+++ scripts/create_database_and_superuser.sh
@@ -2,5 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}/../apiv1/
-python manage.py migrate
-python manage.py createsuperuser --username mycroftai --email mycroftai@localhost --noinput
+python manage migrate
+python manage createsuperuser --username mycroftai --email mycroftai@localhost --noinput
