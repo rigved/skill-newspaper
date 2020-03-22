a810f4b2f5849fabd951806633125711c7bb97ce
Rigved Rakshit <r.phate@gmail.com>
2020-03-22 19:49:49 +0530

    Explicitely activate the Mycroft virtual environment before running any Django admin commands via Python subprocess commands

--- scripts/create_database_and_superuser.sh
+++ scripts/create_database_and_superuser.sh
@@ -2,5 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}/../apiv1/
+source /opt/venvs/mycroft-core/bin/activate
 python manage migrate
 python manage createsuperuser --username mycroftai --email mycroftai@localhost --noinput
--- scripts/update_password_and_token.sh
+++ scripts/update_password_and_token.sh
@@ -4,4 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}
 strings /dev/urandom | grep -o '[[:graph:]]' | head -n 128 | tr -d '\n' | head -c 64 | xargs -0 -I{} ./update_superuser_password.exp {}
 cd ../apiv1/
+source /opt/venvs/mycroft-core/bin/activate
 python manage drf_create_token -r mycroftai
