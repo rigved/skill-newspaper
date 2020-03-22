1cafa01a81603c449f4a03a7657b33bdc7d126c8
Rigved Rakshit <r.phate@gmail.com>
2020-03-22 18:24:56 +0530

    Use correct python multi-line syntax

--- apiv1/manage.py
+++ apiv1/manage.py
@@ -9,9 +9,9 @@ def main():
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        error_string = "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+        error_string = "Couldn't import Django. Are you sure it's installed and \
+            available on your PYTHONPATH environment variable? Did you \
+            forget to activate a virtual environment?"
         raise ImportError(error_string) from exc
     execute_from_command_line(sys.argv)
 
