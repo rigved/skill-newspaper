c370ca4efddb46144db65843cd9638d2a955b945
Rigved Rakshit <r.phate@gmail.com>
2020-03-22 21:12:50 +0530

    Pre-create the required folder

--- scripts/create_database_and_superuser.sh
+++ scripts/create_database_and_superuser.sh
@@ -2,6 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}/../apiv1/
+# Create the required folder
+mkdir -p secrets
 source /opt/venvs/mycroft-core/bin/activate
 python manage migrate
 python manage createsuperuser --username mycroftai --email mycroftai@localhost --noinput
